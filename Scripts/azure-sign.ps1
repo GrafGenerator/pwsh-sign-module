8976decaa2223092c9d2eb17cd5fdee321c03840
Nikita Ivanov <grafgenerator@gmail.com>
2025-07-09 12:30:06 +0700

    sign multiple files at once with azure sign;

--- Scripts/azure-sign.ps1
+++ Scripts/azure-sign.ps1
@@ -17,32 +17,32 @@ $secureSecret = Get-Content "$($ProfilePath -replace '\.json$')-kvs" | ConvertTo
 . $PSScriptRoot\common.ps1
 $clientSecret = Convert-SecureStringToPlainText -SecureString $secureSecret
 
-foreach ($file in $Files) {
-    $signCommand = @(
-        "sign",
-        "--azure-key-vault-url", $signingProfile.keyVaultUrl,
-        "--azure-key-vault-certificate", $signingProfile.certificateName,
-        "--azure-key-vault-tenant-id", $signingProfile.tenantId,
-        "--azure-key-vault-client-id", $signingProfile.clientId,
-        "--azure-key-vault-client-secret", $clientSecret
-    )
-    
-    # Add additional parameters if specified
-    if ($signingProfile.PSObject.Properties.Name -contains "additionalParams" -and -not [string]::IsNullOrWhiteSpace($signingProfile.additionalParams)) {
-        Write-Output "Using additional parameters: $($signingProfile.additionalParams)"
-        $additionalParamsArray = $signingProfile.additionalParams -split ' '
-        $signCommand += $additionalParamsArray
-    }
-    
-    # Add the file to sign
-    $signCommand += $file
-    
-    & $signingProfile.signToolPath $signCommand
+$signCommand = @(
+    "sign",
+    "--azure-key-vault-url", $signingProfile.keyVaultUrl,
+    "--azure-key-vault-certificate", $signingProfile.certificateName,
+    "--azure-key-vault-tenant-id", $signingProfile.tenantId,
+    "--azure-key-vault-client-id", $signingProfile.clientId,
+    "--azure-key-vault-client-secret", $clientSecret
+)
 
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Failed to sign file: $file"
-    }
-    else {
-        Write-Output "Successfully signed file: $file"
-    }
+# Add additional parameters if specified
+if ($signingProfile.PSObject.Properties.Name -contains "additionalParams" -and -not [string]::IsNullOrWhiteSpace($signingProfile.additionalParams)) {
+    Write-Output "Using additional parameters: $($signingProfile.additionalParams)"
+    $additionalParamsArray = $signingProfile.additionalParams -split ' '
+    $signCommand += $additionalParamsArray
+}
+
+# Add the file to sign
+$fileString = $Files -join ' '
+$signCommand += $fileString
+
+$command = "& `"$($signingProfile.signToolPath)`" $signCommand"
+Invoke-Expression $command
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to sign file: $file"
+}
+else {
+    Write-Output "Successfully signed file: $file"
 }
